e9da361b00b27bca8cc73f209e8e223aa4d4c4e6
Kota Nishinaka <knishina@yahoo-corp.jp>
2019-02-01 18:36:48 +0900

    update

--- sample2/main.mjs
+++ sample2/main.mjs
@@ -25,7 +25,7 @@
  */
 async function requestGeoCoderAPI(query) {
   const response = await axios({
-    url: 'https://knishina-eval-test.apigee.net/geocode/V1/geoCoder',
+    url: 'https://knishina-eval-test.apigee.net/yahoo_map/geocode/V1/geoCoder',
     params: {
       query,
       output: 'json',
